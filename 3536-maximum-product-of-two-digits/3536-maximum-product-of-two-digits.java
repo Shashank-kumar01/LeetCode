df203b6c6835d7712f8ee2e2172d1384933cea7e
Shashank-kumar01 <ishashank.kumarr@gmail.com>
2026-07-25 19:32:34 +0530

    Time: 1 ms (100%), Space: 42.8 MB (38.93%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -1,27 +1,36 @@
 class Solution {
     public int maxProduct(int n) {
 
-        ArrayList <Integer> arr = new ArrayList<>();
+        int max1 = Integer.MIN_VALUE ;
+        int max2 = max1 ;
+
+        //ArrayList <Integer> arr = new ArrayList<>();
         while(n != 0)
         {
             int dig = n % 10;
 
-            arr.add(dig) ;
-            n = n/10 ;
-        }
-        int max1 = Integer.MIN_VALUE ;
-        int max2 = max1 ;
-
-        for(int i = 0 ; i < arr.size() ; i++)
-        {
-            if(arr.get(i) >=  max1)
+            if(dig >= max1)
             {
                 max2 = max1 ;
-                max1 = arr.get(i) ;
+                max1 = dig ;
             }
-            if(arr.get(i) > max2 && arr.get(i) != max1)
-                max2 = arr.get(i) ;
+            if(dig > max2 && dig != max1)
+                max2 = dig ;
+            // arr.add(dig) ;
+            n = n/10 ;
         }
+        
+
+        // for(int i = 0 ; i < arr.size() ; i++)
+        // {
+        //     if(arr.get(i) >=  max1)
+        //     {
+        //         max2 = max1 ;
+        //         max1 = arr.get(i) ;
+        //     }
+        //     if(arr.get(i) > max2 && arr.get(i) != max1)
+        //         max2 = arr.get(i) ;
+        // }
         return max1 * max2 ;
         
     }
